d53dce48fa86a305566300b19a52ad249ac40a61
Florent Danguiral <florent.danguiral@protonmail.com>
2025-01-07 19:40:49 +0100

    dfasf

--- scripts/notify-spotify.sh
+++ scripts/notify-spotify.sh
@@ -1,1 +1,14 @@
-/home/flo/.dotfiles/scripts/notify-spotify.sh
+#!/bin/bash
+
+# Boucle infinie pour écouter les changements de piste
+playerctl --player=spotify metadata --format "{{mpris:artUrl}}" --follow | while read -r art_url; do
+   # Récupérer le titre de la chanson et l'artiste
+   song=$(playerctl --player=spotify metadata --format "{{ artist }} - {{ title }}")
+
+   # Télécharger l'image de la pochette de l'album temporairement
+   wget -q "$art_url" -O /tmp/spotify_cover.jpg
+
+   # Envoyer la notification avec l'image de la pochette
+   notify-send "Now playing" "$song" --icon=/tmp/spotify_cover.jpg
+done
+
--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -1,1 +1,17 @@
-/home/flo/.dotfiles/scripts/powermenu.sh
+#!/bin/bash
+
+# Options du menu d'alimentation
+OPTIONS="襤 Éteindre\n Redémarrer\n鈴 Suspendre\n Déconnexion"
+
+# Affiche le menu avec Rofi
+CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -i -p "Choisir une action:")
+
+# Exécute la commande correspondante
+case "$CHOICE" in
+    *Éteindre) systemctl poweroff ;;
+    *Redémarrer) systemctl reboot ;;
+    *Suspendre) systemctl suspend ;;
+    *Déconnexion) pkill -KILL -u $USER ;;
+    *) exit 1 ;;  # Quitte si aucune option valide n'est sélectionnée
+esac
+
