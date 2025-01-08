f02a3bd97f96555aa1b0e7e5395eae087dabf8a6
Florent Danguiral <florent.danguiral@protonmail.com>
2025-01-08 09:53:58 +0100

    adf

---
+++ eldritch/colors.sh
@@ -0,0 +1,37 @@
+# colors.sh
+
+# Markdown headings
+export COLOR18="#5b4996" # Markdown heading 2
+export COLOR19="#21925b" # Markdown heading 3
+export COLOR20="#027d95" # Markdown heading 4
+export COLOR21="#585c89" # Markdown heading 5
+export COLOR22="#0f857c" # Markdown heading 6
+export COLOR23="#396592"
+
+# General colors
+export COLOR04="#987afb"
+export COLOR02="#37f499"
+export COLOR03="#04d1f9"
+export COLOR01="#949ae5"
+export COLOR05="#19dfcf"
+export COLOR08="#5fa9f4"
+export COLOR06="#1682ef"
+
+# Markdown and UI elements
+export CODEBLOCK="#1c242f"     # Markdown codeblock
+export COMMENT="#a5afc2"       # Comments
+export BACKGROUND="#1e1e2e"    # Terminal and neovim background
+
+# Underlines
+export UNDERLINE_SPELLBAD="#f16c75"
+export UNDERLINE_SPELLCAP="#f1fc79"
+
+# Cursor and selection
+export CURSOR_LINE="#314154"
+export CURSOR_TEXT="#ebfafa"
+export TMUX_INACTIVE="#013e4a"
+export SELECTED_TEXT="#e9b3fd"
+export LUALINE_ACROSS="#141b22"
+export CURSOR_COLOR="#F712FF"
+export INACTIVE_BG="#232e3b"
+
